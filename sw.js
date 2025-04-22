7f1d86a52cfb3165d394880768a56fd7263b1372
MasterGameZone <mastersunil1451@gmail.com>
2025-04-22 14:01:53 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,36 @@
+const CACHE_NAME = 'random-number-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192x192.png',
+  '/icon-512x512.png'
+];
+
 self.addEventListener('install', event => {
-  console.log('Service Worker installed');
+  console.log('Service Worker: Installed');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
 self.addEventListener('activate', event => {
-  console.log('Service Worker activated');
+  console.log('Service Worker: Activated');
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) return caches.delete(cache);
+        })
+      );
+    })
+  );
 });
 
-self.addEventListener('fetch', function(event) {
-  // Can cache things here later
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
